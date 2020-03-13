3fef335d38bb88f3caea6608795d7ae864384e6c
vacoool <alexander.vakulyuk@gmail.com>
2020-03-13 11:54:38 +0300

    RandomFieldGenerator.cs added

---
+++ src/Services/RandomFieldGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System.Security.Cryptography;
+
+namespace thegame.Services
+{
+    public class RandomFieldGenerator
+    {
+        public static int[,] Create(int width, int height, int colorCount)
+        {
+            var result = new int[width, height];
+            var buffer = new byte[width * height];
+            using (var generator = RandomNumberGenerator.Create())
+            {
+                generator.GetBytes(buffer);
+            }
+
+            for (var i = 0; i < buffer.Length; i++)
+            {
+                result[i / height, i % height] = buffer[i] % colorCount;
+            }
+            return result;
+        }
+    }
+}
