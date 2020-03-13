f525cb726802f836b793cf1ecdbda76da430a022
Tsirikov <tsirikov.s.a@gmail.com>
2020-03-13 11:37:48 +0300

    GameBoard`s method to get color of cell.

--- src/Models/GameBoard.cs
+++ src/Models/GameBoard.cs
@@ -13,6 +13,19 @@ namespace thegame.Models
             Content = content;
         }
 
+        public int GetColorOfCell(Vec vec)
+        {
+            if (0 <= vec.X && vec.X < SizeX
+             && 0 <= vec.Y && vec.Y < SizeY)
+            {
+                return Content[vec.X, vec.Y];
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+        }
+
         public int SizeX;
         public int SizeY;
         public int[,] Content;
