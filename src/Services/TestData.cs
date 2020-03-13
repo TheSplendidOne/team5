c0db2f3e66e40b13948a28a10f3f8f877b91e046
Tsirikov <tsirikov.s.a@gmail.com>
2020-03-13 13:22:07 +0300

    Add complexity level.

--- src/Services/TestData.cs
+++ src/Services/TestData.cs
@@ -5,11 +5,28 @@ namespace thegame.Services
 {
     public class TestData
     {
-        public static GameDto AGameDto(Vec movingObjectPosition)
+        public static GameDto AGameDto(Vec movingObjectPosition, int complexityLevel)
         {
-            var width = 10;
-            var height = 10;
-            var colorCount = 6;
+            int width, height, colorCount;
+            switch (complexityLevel)
+            {
+                case 2:
+                    width = 20;
+                    height = 20;
+                    colorCount = 12;
+                    break;
+                case 1:
+                    width = 15;
+                    height = 15;
+                    colorCount = 9;
+                    break;
+                case 0:
+                default:
+                    width = 10;
+                    height = 10;
+                    colorCount = 6;
+                    break;
+            }
 
             var boardData = RandomFieldGenerator.Create(width, height, colorCount);
             var testBoard = new GameBoard(width, height,  boardData, colorCount);
