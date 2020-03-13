b3031092b92ed9b8caefbcdea6c404a738350859
Tsirikov <tsirikov.s.a@gmail.com>
2020-03-13 11:23:52 +0300

    Board class.

---
+++ src/Models/GameBoard.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace thegame.Models
+{
+    class GameBoard
+    {
+        public GameBoard(int sizeX, int sizeY, int[,] content)
+        {
+            SizeX = sizeX;
+            SizeY = sizeY;
+            Content = content;
+        }
+
+        public int SizeX;
+        public int SizeY;
+        public int[,] Content;
+    }
+}
