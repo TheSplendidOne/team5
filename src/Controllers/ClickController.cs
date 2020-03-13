ba86af61d701eb714d08df53105268a99b29781d
Sinichenko Konstantin <k.sinichenko@yandex.ru>
2020-03-13 13:27:19 +0300

    Add clicks processing

--- src/Controllers/ClickController.cs
+++ src/Controllers/ClickController.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using thegame.Models;
@@ -14,7 +15,21 @@ namespace thegame.Controllers
         {
             var game = TestData.AGameDto(userInput.ClickedPos ?? new Vec(1, 1));
             if (userInput.ClickedPos != null)
-                game.Cells.First(c => c.Type == "");
+            {
+                IReadOnlyList<String> palette = ColorPaletteGenerator.CreateHexPalette();
+                Int32[,] cells = game.Cells.ToIndexArray(palette.ToArray());
+                List<Vec> adjacentCells = DFS.GetAdjacentCells(
+                    new Boolean[game.Width, game.Height],
+                    userInput.ClickedPos,
+                    new GameBoard(game.Width, game.Height,
+                        cells, palette),
+                    cells[0, 0]);
+
+                foreach (CellDto cell in game.Cells.Where(cell => adjacentCells.Contains(cell.Pos)))
+                {
+                    cell.Type = palette[cells[userInput.ClickedPos.X, userInput.ClickedPos.Y]];
+                }
+            }
             return new ObjectResult(game);
         }
     }
